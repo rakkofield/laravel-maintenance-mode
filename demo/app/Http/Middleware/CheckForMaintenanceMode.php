62455b85d842e3add33a4d5ad2a9bd86e0960c07
Naoki Tsuchiya <ntsuchiya1105@gmail.com>
2024-11-09 01:55:39 +0900

    Use package

--- demo/app/Http/Middleware/CheckForMaintenanceMode.php
+++ demo/app/Http/Middleware/CheckForMaintenanceMode.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Middleware;
 
-use Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode as Middleware;
+use RakkoInc\LaravelMaintenanceMode\Foundation\Http\Middleware\CheckForMaintenanceMode as Middleware;
 
 class CheckForMaintenanceMode extends Middleware
 {
